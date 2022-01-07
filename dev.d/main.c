130fddc78e923dc4989c4661c8cd2e670d2a61ff
etto48 <minecraft.etto@gmail.com>
2022-01-07 22:12:13 +0100

    very minor refactoring

--- dev.d/main.c
+++ dev.d/main.c
@@ -10,13 +10,9 @@ int main(int argc, char **argv)
     uint16_t device_port = atoi(argv[1]);
     Startup();
     if(argc == 3)
-    {
         NetworkServerAddress = inet_addr(argv[2]);
-    }
     else
-    {
         NetworkServerAddress = inet_addr(SERVER_DEFAULT_ADDRESS);
-    }
     printf("Device started on port %u\n", device_port);
     printf("Type \"help\" for a list of commands\n");
     NetworkMainLoop(device_port);
