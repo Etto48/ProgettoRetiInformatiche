3836db3984887809a4da71d778c6ff06d3e4fc6e
etto48 <minecraft.etto@gmail.com>
2022-01-08 01:19:21 +0100

    DNS server ip

--- dev.d/main.c
+++ dev.d/main.c
@@ -4,13 +4,14 @@ int main(int argc, char **argv)
 {
     if (argc < 2 || argc > 3)
     {
-        printf("Usage: ./dev <listening port> [server ip]\n");
+        printf("Usage: ./dev <listening port> [server hostname|ip]\n");
         return -1;
     }
     uint16_t device_port = atoi(argv[1]);
     Startup();
-    if(argc == 3)
-        NetworkServerAddress = inet_addr(argv[2]);
+    struct hostent* server_host;
+    if(argc == 3 && (server_host = gethostbyname(argv[2])))
+        NetworkServerAddress = *(uint32_t*)(server_host->h_addr_list[0]);
     else
         NetworkServerAddress = inet_addr(SERVER_DEFAULT_ADDRESS);
     printf("Device started on port %u\n", device_port);
--- dev.d/main.h
+++ dev.d/main.h
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <netdb.h>
 #include "../global.d/sha256/sha256.h"
 #include "../global.d/debug/debug.h"
 #include "command_parser/command_parser.h"
